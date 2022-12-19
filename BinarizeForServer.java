1a4cb7011a30289a94d37a82387ba02abaed7ca1
Nikesh DC <nikeshdc111@gmail.com>
2022-12-19 21:54:06 +0545

    fixed bug of segmentation and imageHistogram

--- BinarizeForServer.java
+++ BinarizeForServer.java
@@ -26,13 +26,10 @@ public class BinarizeForServer
         Segmentation segmentation = new Segmentation(dilatedImage);
         segmentation.segment();
         
-        System.out.println("Binarizing using otsu");
         Binarization otsuBound = new OtsuBounds(srcimg, segmentation.getComponents());
         otsuBound.binarize();
-        System.out.println("Binarized using otsu");
         
         //save binarized image
-        System.out.println("Writing image");
         ImageUtility.writeImage(otsuBound.getBinarizedImage(), savepath);
     }
 }
