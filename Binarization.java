22ecdf37cf1c47cc633fc5f663c293ace46511ea
Nikesh DC <nikeshdc111@gmail.com>
2022-12-22 12:33:26 +0545

    fixed bug for simple-threshold
    checks image to see if conversion to grayscale is needed

--- Binarization.java
+++ Binarization.java
@@ -5,19 +5,24 @@
         
        public abstract void binarize();
        
-       public void setImage(Image _srcImage)
+       private static void rectifyImage(Image image)
        {
-           sourceImage = _srcImage;
-           //convert rgb image to grayscale
-           if(sourceImage.getType() == Image.TYPE.RGB)
+           if(image.getType() == Image.TYPE.RGB)
            {   
-               System.out.println("Converted RGB image to gary for binarization");
-               ImageUtility.convertRGB2gray(_srcImage);
+               //System.out.println("Converted RGB image to gary for binarization");
+               ImageUtility.convertRGB2gray(image);
            }
-           else if(sourceImage.getType() == Image.TYPE.BIN)
+           else if(image.getType() == Image.TYPE.BIN)
            {   
                System.out.println("Source image for binarization in binary format!!");
            }
+       }
+       
+       public void setImage(Image _srcImage)
+       {
+           sourceImage = _srcImage;
+           //convert rgb image to grayscale if necessary
+           rectifyImage(_srcImage);
            
            binarizedImage = new Image(sourceImage.getWidth(), sourceImage.getHeight());
            binarizedImage.setType(Image.TYPE.BIN);
@@ -26,15 +31,18 @@
        public Image getBinarizedImage()
        {return binarizedImage;}
        
-       public static Image simpleThreshold(Image _image, int threshold)
+       public static Image simpleThreshold(Image image, int threshold)
         {
-            Image binarizedImage = new Image(_image.getWidth(), _image.getHeight());
+            //convert rgb image to grayscale if necessary
+            rectifyImage(image);
+           
+            Image binarizedImage = new Image(image.getWidth(), image.getHeight());
             binarizedImage.setType(Image.TYPE.BIN);
-            for (int i=0; i < _image.getWidth(); i++)
+            for (int i=0; i < image.getWidth(); i++)
             {
-                for(int j=0; j < _image.getHeight(); j++)
+                for(int j=0; j < image.getHeight(); j++)
                 {
-                    if (_image.pixel[i][j] < threshold)
+                    if (image.pixel[i][j] < threshold)
                         {binarizedImage.pixel[i][j] = 1;}
                     else
                         {binarizedImage.pixel[i][j] = 0;}
