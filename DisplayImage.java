d8dc4631f6d6c43bcdb03cc43af9acf94babf72c
Nikesh DC <nikeshdc111@gmail.com>
2023-01-03 14:35:15 +0545

    seperated function for displaying image in new window or existing window

--- DisplayImage.java
+++ DisplayImage.java
@@ -4,22 +4,59 @@ public class DisplayImage
 {
    public static int DEFAULT_HEIGHT = 600;
    
+   static JFrame window;
+   static ImagePanel imagePanel;
+   
    public static void display(Image image, int width, int height)
+   {
+       if(window == null)
+       {
+           window = new JFrame();
+           window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+       }
+       if(imagePanel == null)
+       {
+           imagePanel = new ImagePanel();
+           window.getContentPane().add(imagePanel);
+       }
+       imagePanel.setImg(image);
+       imagePanel.setBounds(0,0,width, height);
+       window.setSize(width, height);
+       window.setVisible(true);
+   }
+   public static void display(Image image)
+   {
+       int height = DEFAULT_HEIGHT;
+       int width = (int)(height * (float)image.getWidth() / image.getHeight());
+       display(image, width, height);
+   }
+   public static void closeDisplay()
+   {
+       window = null;
+       imagePanel = null;
+   }
+   public static void hideDisplay()
+   {
+       window.setVisible(false);
+   }
+   
+   
+   public static void displayNew(Image image, int width, int height)
    {//height and width represent the window's height and width and not of image
        JFrame window = new JFrame();
        ImagePanel myimgpanel = new ImagePanel();
        myimgpanel.setImg(image);
        myimgpanel.setBounds(0,0,width, height);
        window.getContentPane().add(myimgpanel);
-       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
        window.setSize(width, height);
        window.setVisible(true);
    }
    
-   public static void display(Image image)
+   public static void displayNew(Image image)
    {
        int height = DEFAULT_HEIGHT;
        int width = (int)(height * (float)image.getWidth() / image.getHeight());
-       display(image, width, height);
+       displayNew(image, width, height);
    }
 }
