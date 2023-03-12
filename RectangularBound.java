de3be47c1b523bd80b43104f0d37f7132f5e3e3e
NikeshDC <nikeshdc111@gmail.com>
2023-03-12 18:16:21 +0545

    added validity boolean
    a boolean attribute can be used to invalidate it and invalidity can be checked by specific programs for specific purposes

--- RectangularBound.java
+++ RectangularBound.java
@@ -4,4 +4,8 @@ public class RectangularBound<T>
     public T maxX;  //ending coord along x-axis
     public T minY;  //starting coord along y-axis
     public T maxY;  //ending coord along y-axis
+    private boolean validity = true;
+    
+    public boolean isValid() {return validity;}
+    public void invalidate() { validity = false;}
 }
