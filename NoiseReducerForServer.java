095b4bfa7d1265e6d10f199e1de9f369b466c87b
Ravi Pandey <59216046+ravip5921@users.noreply.github.com>
2023-03-16 10:36:52 +0545

    Update NoiseReducerForServer.java

--- NoiseReducerForServer.java
+++ NoiseReducerForServer.java
@@ -1,7 +1,7 @@
 public class NoiseReducerForServer {
     public static void main(String args[]) {
         reduceNoiseAndSave(args[0], args[1]);
-        // reduceNoiseAndSave("bin_img (15).png", "denoised_img_15.png");
+//         reduceNoiseAndSave("bin_img (15).png", "denoised_img_15.png");
     }
 
     public static void reduceNoiseAndSave(String imagepath, String savepath) {
@@ -10,8 +10,11 @@ public class NoiseReducerForServer {
         Image binarizedImage = Binarization.simpleThreshold(srcimg, 128);
 
         NoiseReducer noiseRed = new NoiseReducer(binarizedImage, 0.005f, 0.015f, 0.2f, 0.1f, 0.05f, 0.005f);
-        Image denoisedImage2 = noiseRed.getCleanImage();
+        Image denoisedImage = noiseRed.getCleanImage();
         // noiseRed.removeProbableImages(denoisedImage2);
+
+        NoiseReducer noiseRed2 = new NoiseReducer(denoisedImage, 0.005f, 0.015f, 0.2f, 0.1f, 0.05f, 0.005f);
+        Image denoisedImage2 = noiseRed2.getCleanImage2();
 
         // save binarized image
         ImageUtility.writeImage(denoisedImage2, savepath);
